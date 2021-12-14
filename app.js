4162b45badc711f02f8bce46e75ab25bd81c6174
Melina Joloidovsky B <melina.jb@hotmail.com>
2021-12-14 12:57:35 -0300

    rutas js y hbs

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var tipsRouter = require('./routes/tips');
+var propuestasRouter = require('./routes/propuestas');
+var nosotrosRouter = require('./routes/nosotros');
+var contactoRouter = require('./routes/contacto');
 
 var app = express();
 
@@ -20,7 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/tips', tipsRouter);
+app.use('/propuestas', propuestasRouter);
+app.use('/nosotros', nosotrosRouter);
+app.use('/contacto', contactoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
